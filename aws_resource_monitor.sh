85b296fe2f560f35dcd6c69df1a14df442ae1b3c
jayantkjh <jayantkumarkjh123@gmail.com>
2026-01-06 09:53:20 +0530

    Version 2

--- aws_resource_monitor.sh
+++ aws_resource_monitor.sh
@@ -2,32 +2,53 @@
 #############################
 # Author: Jayant Kumar Pathak
 # Date : 6th Jan 2026
-# Version : v1
+# Version : v2
 ####################################################
 # This shell script will monitor AWS resource usage
 ####################################################
 
 # AWS S3
 # AWS EC2
-# AWS Lambda 
+# AWS Lambda
 # AWS IAM user
 
-set -x 
-# List AWS S3 buckets
-echo "List S3 buckets"
-aws s3 ls
+set -e
+set -o pipefail
 
-# List Ec2 Instances
-echo "list EC2 Instances"
-aws ec2 describe-instances --region us-east-1 | jq '.Reservations[].Instances[].InstanceId'
+echo "=================================================="
+echo "AWS RESOURCE MONITORING DASHBOARD"
+echo "=================================================="
+echo ""
 
-# List Lambda function
-echo "List Lambda function"
-aws lambda list-functions | jq '.Functions[].FunctionName'
+# List AWS S3 buckets with size and object count
+echo "========== S3 BUCKETS =========="
+echo ""
+aws s3api list-buckets --output json | jq -r '.Buckets[] | .Name' | while read bucket; do
+    aws s3api list-objects-v2 --bucket "$bucket" --output json | jq -r --arg bucket "$bucket" '
+        (.Contents | map(.Size) | add // 0) as $size |
+        (.Contents | length) as $count |
+        "Bucket: \($bucket) | Size: \($size/1024/1024 | floor) MB | Objects: \($count)"
+    '
+done
+echo ""
 
-# List IAM user 
-echo "List IAM user"
+# List EC2 Instances with status and instance type
+echo "========== EC2 INSTANCES =========="
+echo ""
+aws ec2 describe-instances --region us-east-1 --output json | jq -r '.Reservations[].Instances[] | "Instance ID: \(.InstanceId) | Status: \(.State.Name) | Type: \(.InstanceType)"'
+echo ""
 
-aws iam list-users | jq '.Users[].UserName'
+# List Lambda functions with runtime/language
+echo "========== LAMBDA FUNCTIONS =========="
+echo ""
+aws lambda list-functions --region us-east-1 --output json | jq -r '.Functions[] | "Function Name: \(.FunctionName) | Runtime: \(.Runtime)"'
+echo ""
 
-
+# List IAM users with last access time
+echo "========== IAM USERS =========="
+echo ""
+aws iam list-users --output json | jq -r '.Users[] | "User: \(.UserName) | Created: \(.CreateDate)"'
+echo ""
+echo "=================================================="
+echo "END OF REPORT"
+echo "=================================================="
